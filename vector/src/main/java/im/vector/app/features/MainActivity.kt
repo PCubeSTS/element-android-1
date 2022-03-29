ce62f73b1cee6d39c4f85b8a9b9a53d76b6e4d2e
PCubeSTS <agarwaltanmay95@gmail.com>
2022-03-29 13:57:14 +0530

    d650

--- vector/src/main/java/im/vector/app/features/MainActivity.kt
+++ vector/src/main/java/im/vector/app/features/MainActivity.kt
@@ -118,29 +118,6 @@ class MainActivity : VectorBaseActivity<ActivityMainBinding>(), UnlockedActivity
         } else {
             startNextActivityAndFinish()
         }
-
-//        supportActionBar?.hide()
-//        @Suppress("DEPRECATION")
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            window.insetsController?.hide(WindowInsets.Type.statusBars())
-//        } else {
-//            window.setFlags(
-//                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                    WindowManager.LayoutParams.FLAG_FULLSCREEN
-//            )
-//        }
-
-
-
-//        window.decorView.apply {
-//            // Hide both the navigation bar and the status bar.
-//            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-//            // a general rule, you should design your app to hide the status bar whenever you
-//            // hide the navigation bar.
-//            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-//        }
-
-
     }
 
 
